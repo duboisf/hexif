5943b58e8a171c57e27a240315fd4f45d5ce9c57
Fred Dubois <fred.dubois@gmail.com>
2012-09-16 07:57:42 -0400

    Added App newtype

--- main.hs
+++ main.hs
@@ -1,4 +1,26 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Main where
+
+import Control.Monad.Reader
+
+data AppConfig = AppConfig {
+    acSrcDir :: FilePath
+  , acTargetDir :: FilePath
+  }
+
+newtype App a = App {
+    runA :: ReaderT AppConfig IO a
+  } deriving (Monad, MonadIO, MonadReader AppConfig)
+
+-- get list of files from source folder
+getFiles :: App FilePath
+getFiles = undefined
+
+-- for each file f:
+--   extract modification date modDate
+--   build destFolder from modDate (targetDir/year/month)
+--   if not exists destFolder, create it
+--   copy f to destFolder
 
 main :: IO ()
 main = putStrLn "Hullo, world"
