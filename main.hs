dde3c6964aac06907bd7fd49aeab4d0f50cd6bd4
Fred Dubois <fred.dubois@gmail.com>
2012-10-18 17:04:19 -0400

    Added runApp, worked on getFiles

--- main.hs
+++ main.hs
@@ -1,7 +1,9 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Main where
 
+import Control.Monad (liftM)
 import Control.Monad.Reader
+import System.Directory (getDirectoryContents)
 
 data AppConfig = AppConfig {
     acSrcDir :: FilePath
@@ -12,9 +14,16 @@ newtype App a = App {
     runA :: ReaderT AppConfig IO a
   } deriving (Monad, MonadIO, MonadReader AppConfig)
 
+runApp :: App a -> AppConfig -> IO a
+runApp app config = runReaderT (runA app) config
+
 -- get list of files from source folder
-getFiles :: App FilePath
-getFiles = undefined
+getFiles :: App [FilePath]
+getFiles = do
+  srcDir <- acSrcDir `liftM` ask
+  rawEntries <- liftIO $ getDirectoryContents srcDir
+  let entries = filter (not . flip elem [".", ".."]) rawEntries
+  return []
 
 -- for each file f:
 --   extract modification date modDate
