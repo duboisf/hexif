11985b86890fd7c15494e1c9ace16af20c30c8ff
Fred Dubois <fred.dubois@gmail.com>
2012-10-18 21:06:13 -0400

    Get all files recursively works
    Need to fix case where you can get a permission denied. This throws an
    exception and bails straight out of the function. Need to catch a
    permission denied on a folder and just continue.

--- main.hs
+++ main.hs
@@ -3,11 +3,16 @@ module Main where
 
 import Control.Monad (liftM)
 import Control.Monad.Reader
-import System.Directory (getDirectoryContents)
+import System.Directory (doesDirectoryExist, doesFileExist, getDirectoryContents)
+import System.FilePath ((</>))
+
+data File = File FilePath deriving Show
+
+data Dir = Dir FilePath deriving Show
 
 data AppConfig = AppConfig {
-    acSrcDir :: FilePath
-  , acTargetDir :: FilePath
+    acSrcDir :: Dir
+  , acTargetDir :: Dir
   }
 
 newtype App a = App {
@@ -18,13 +23,48 @@ runApp :: App a -> AppConfig -> IO a
 runApp app config = runReaderT (runA app) config
 
 -- get list of files from source folder
-getFiles :: App [FilePath]
+getFiles :: App [File]
 getFiles = do
   srcDir <- acSrcDir `liftM` ask
-  rawEntries <- liftIO $ getDirectoryContents srcDir
-  let entries = filter (not . flip elem [".", ".."]) rawEntries
-  return []
+  liftIO $ getAllFilesRecursively srcDir
 
+filterDirEntries :: [FilePath] -> [FilePath]
+filterDirEntries = filter (not . flip elem [".", ".."])
+
+getFilteredDirContents :: Dir -> IO [FilePath]
+getFilteredDirContents (Dir dir) =
+  (map (dir </>) . filterDirEntries) `liftM` getDirectoryContents dir
+  
+partitionFileEntries :: [FilePath] -> IO ([Dir], [File])
+partitionFileEntries entries =
+  part entries [] []
+  where
+    part :: [FilePath] -> [Dir] -> [File] -> IO ([Dir], [File])
+    part [] dirs files = return (dirs, files)
+    part (entry:entries) dirs files = do
+      isDir <- doesDirectoryExist entry
+      if isDir
+        then part entries (Dir entry : dirs) files
+        else do
+          isFile <- doesFileExist entry 
+          if isFile
+            then part entries dirs (File entry : files)
+            else part entries dirs files
+
+getPartitionedDirContents :: Dir -> IO ([Dir], [File])
+getPartitionedDirContents dir =
+  getFilteredDirContents dir >>= partitionFileEntries
+
+getAllFilesRecursively :: Dir -> IO [File]
+getAllFilesRecursively dir =
+  getFiles' [dir] []
+  where
+    getFiles' :: [Dir] -> [File] -> IO [File]
+    getFiles' [] files = return files
+    getFiles' (dir:dirs) files = do
+      (newDirs, newFiles) <- getPartitionedDirContents dir
+      getFiles' (dirs ++ newDirs) (files ++ newFiles)
+  
 -- for each file f:
 --   extract modification date modDate
 --   build destFolder from modDate (targetDir/year/month)
